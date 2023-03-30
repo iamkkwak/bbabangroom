df7a7bd0ca4c93521478ee27ace97fbba4fe4884
iamkkwak <iamkkwak@gmail.com>
2023-03-30 20:58:37 +0900

    Feature: Hide passed time

--- src/components/price-time-select/PriceTime.utils.ts
+++ src/components/price-time-select/PriceTime.utils.ts
@@ -1,6 +1,12 @@
 import { dayjs, formatTime } from '@utils/dayjs.utils';
 import { map } from 'lodash';
 
+type Truthy<T> = T extends false | '' | 0 | null | undefined ? never : T; // from lodash
+
+function truthy<T>(value: T): value is Truthy<T> {
+  return !!value;
+}
+
 export const getTimeData = () => {
   const selectedDate = window.sessionStorage.getItem('price-date');
 
@@ -9,10 +15,11 @@ export const getTimeData = () => {
     const disabled = dayjs(selectedDate).add(index, 'hour').isBefore(dayjs())
       ? true
       : false;
-    return {
-      value: formattedTime,
-      label: formattedTime,
-      disabled,
-    };
-  });
+
+    if (!disabled)
+      return {
+        value: formattedTime,
+        label: formattedTime,
+      };
+  }).filter(truthy);
 };
